1cc1d9f857c703c87424416f44a6d4bcad6e80ea
Dharana418 <thiyunuwan567@gmail.com>
2025-09-10 15:40:04 +0530

    sdasdasdsa

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authroutes');
 
 const app = express();
 
@@ -17,3 +18,4 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
