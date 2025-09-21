ebf8a2289ce4cc571716a07ac3d2fc194a82b3ca
Dharana418 <thiyunuwan567@gmail.com>
2025-09-21 19:09:12 +0530

    d

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const authRoutes = require('./routes/authroutes');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const dbURI = 'mongodb+srv://admin:6kkAhPcJW5SCtWET@agrovista.8l8dq.mongodb.net/node-auth?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,5 +14,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use('/auth', authRoutes);
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res) => res.status(404).render('404'));
+// app.listen(3000, () => console.log('Server is running on port 3000'));
