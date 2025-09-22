17a024104b630deb8a4c296edf6fe9c313c0b3af
Dharana418 <thiyunuwan567@gmail.com>
2025-09-22 22:18:09 +0530

    validations

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const { type } = require('os');
+const {isEmail}=require('validator');
 
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
-        required:true,
-        unique:true,
-        lowercase:true
+        required:[true,'please enter an email'],
+        unique:[true,'that email is already registered'],
+        lowercase:true,
+        validate:[isEmail,'please enter a valid email']
     },
     password:{
         type:String,
-        required:true,
-        minlength:6
+        required:[true,'please enter a password'],
+        minlength:[6,'minimum password length is 6 characters']
     },
     createdAt:{
         type:Date,
