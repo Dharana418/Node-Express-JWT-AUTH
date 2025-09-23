1a365b2b789210a0cb963c436a690690b16e8bc2
Dharana418 <thiyunuwan567@gmail.com>
2025-09-23 16:42:48 +0530

    cookies-parser

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/auth', authRoutes);
 
 //cookies
 app.get('/set-cookies', (req, res) => {
-  res.setHeader('Set-Cookie','newUser=true');
+  //res.setHeader('Set-Cookie','newUser=true');
   res.cookie('newUser', false);
   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
   res.send('you got the cookies!');
